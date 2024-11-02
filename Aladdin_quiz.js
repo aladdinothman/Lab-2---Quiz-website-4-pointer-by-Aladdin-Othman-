613ad03c48a114b72d26cdd2cab14207c4341cbd
aladdinothman <aladdinothman@outlook.com>
2024-11-02 21:55:28 +0100

    V14

--- Aladdin_quiz.js
+++ Aladdin_quiz.js
@@ -50,7 +50,7 @@ if (question1Answer && question1Answer.value === correctAnswers.question1) {
 const selectedCities=
 Array.from(document.query.selectorAL('Input[name="question2"]:checked')).map(el => el.value);
 if (arraysEqual(selectedCities, correctAnswers.question2)) {
-    scorre++;
+    score++;
 }
 
 const question4Answer= document.querySelector('input[name="question4"]:checked'); 
@@ -65,16 +65,37 @@ if (question6Answer && question6Answer.value === correctAnswers.question6) {
 
 const question7Answer=document.querySelector('input[name="question7"]:checked');
 if (question7Answer && question7Answer.value === correctAnswers.question7) {
-    scorre++;
+    score++;
 }
 
 const question8answer= document.querySelector('input[name="question8"]:checked');
 if (question8Answer && question8Answer.value === correctAnswers.question8) {
-    scorre++; 
+    score++; 
 }
 
 const question9Answer=document.querySelector('Input[name="question9"]:checked'); 
 if (question9Answer && question9Answer.value === correctAnswers.question9) {
-    scorre++;
+    score++;
 }
+
+document.getElementById('result').innerHTML = `
+<div>
+<h2>Results of the Quiz</h2>
+<p>${firstName} ${lastName}, your score is: ${score}/7</p> 
+<p>Correct Answers</p>
+<ul>
+    <li>1.Capital of Sweden: ${correctAnswers.question1}</li>
+    <li>2.Swedish cities: ${correctAnswers.question2}</li>
+    <li>4.Sweden joined EU in: ${correctAnswers.question4}</li>
+    <li>6.The official language of Sweden: ${correctAnswers.question6}</li>
+    <li>7.Swedish furniture company: ${correctAnswers.question7} </li>
+    <li>8.The population of Sweden: ${correctAnswers.question8} </li>
+    <li>9.The amount of members that sit in Sweden's parliament: ${correctAnswers.question9} </li>
+
+</ul>
+</div>
+`;
+});
+
+function
 
